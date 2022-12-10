aba39b10f53c324a4ac901ce9bdaa18bedc71236
MateDemn <demonicskyers@gmail.com>
2022-12-10 16:26:17 +0100

    Changed database engine to sqlite

--- DBManager.py
+++ DBManager.py
@@ -9,15 +9,15 @@ class DBManager:
     """
     def __init__(self):
         self.Base = declarative_base()
-        if 'DATABASE_URL' in os.environ:
-            uri = os.environ['DATABASE_URL']
-            # this is very stupid, but isn't work otherwise
-            if uri.startswith("postgres://"):
-                uri = uri.replace("postgres://", "postgresql://", 1)
-            self.engine = create_engine(uri, future=True, echo=True)
-        else:
-            self.engine = create_engine('postgresql://viuaugsugdvrpv:073c54d58241d4959b7764fea64fb39a28a1a99e1b109bb5c043d3eca14f5e00@ec2-34-246-24-110.eu-west-1.compute.amazonaws.com:5432/d7joef1t7h2s8v', future=True, echo=True)
-        #self.engine = create_engine('sqlite://', future=True, echo=True)
+        #if 'DATABASE_URL' in os.environ:
+        #    uri = os.environ['DATABASE_URL']
+        #    # this is very stupid, but isn't work otherwise
+        #    if uri.startswith("postgres://"):
+        #        uri = uri.replace("postgres://", "postgresql://", 1)
+        #    self.engine = create_engine(uri, future=True, echo=True)
+        #else:
+        #    self.engine = create_engine('postgresql://viuaugsugdvrpv:073c54d58241d4959b7764fea64fb39a28a1a99e1b109bb5c043d3eca14f5e00@ec2-34-246-24-110.eu-west-1.compute.amazonaws.com:5432/d7joef1t7h2s8v', future=True, echo=True)
+        self.engine = create_engine('sqlite://', future=True, echo=True)
         self.Session = sessionmaker(bind=self.engine, future=True)
 
 dbmanager = DBManager()
